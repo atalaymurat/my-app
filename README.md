0a26b0fc0e6a9c768ef699ba4ce192beea41107a
atalaymurat <atalaymurat@msn.com>
2025-04-07 11:02:41 +0300

    nextjs and tailwind

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # My - App
 Try to develop a functional SaaS Project
+
+## Backend
+Express Server
---
+++ frontend/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="w-3xl px-10 py-4 border border-gray-300">
+      <div className="text-3xl font-bold text-red-800">Postiva</div>
+    </div>
+  );
+}
