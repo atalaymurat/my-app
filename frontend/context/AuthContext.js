16c771cbbe48eb7d35fe847caa2cf0e482ed2be2
atalaymurat <atalaymurat@msn.com>
2025-04-11 14:04:24 +0300

    change mistakes

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -10,7 +10,7 @@ import axios from "axios";
 const apiClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_API_URL // Make sure this env var is set in production
+      ? process.env.NEXT_PUBLIC_BACKEND_URL // Make sure this env var is set in production
       : "http://localhost:5000", // Your backend URL for development
   withCredentials: true, // Crucial for sending/receiving HTTP-only cookies
   headers: {
