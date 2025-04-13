278e1a5ab88180b61ca788150cc7ec6b38decd72
atalaymurat <atalaymurat@msn.com>
2025-04-13 23:20:04 +0300

    MIDDLEWARE CONSOLE LOG

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -2,11 +2,9 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const token = request.cookies.get("token")?.value;
+  console.log("Token:", token);
 
   // If there's no token, redirect to login
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
 
   // If there's a token, allow the request to proceed
   return NextResponse.next();
