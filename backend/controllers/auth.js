5de4be2adfa4cdf865d495910d732b18d63f7526
atalaymurat <atalaymurat@msn.com>
2025-04-13 14:56:38 +0300

    cookies sets

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -16,9 +16,6 @@ const getCookieOptions = () => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    path: "/",
-    maxAge: 5 * 24 * 60 * 60 * 1000, // 5 days
-    partitioned: true,
   };
 
   const options = isProduction ? optionsServer : optionsLocal;
