9632c71194cfbb292c90fcd857a2eec4635a0c3a
atalaymurat <atalaymurat@msn.com>
2025-04-13 23:45:13 +0300

    full headers

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -3,7 +3,13 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   console.log("\n--- Middleware Execution Started ---");
   console.log("Request URL:", request.url);
-  console.log("Request Headers:", JSON.stringify(request, null, 2));
+  
+  // Log all request headers
+  const headers = {};
+  request.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+  console.log("Request Headers:", JSON.stringify(headers, null, 2));
 
   // Get cookies from headers
   const cookieHeader = request.headers.get('cookie');
