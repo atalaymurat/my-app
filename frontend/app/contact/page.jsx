ad4b8c8c6b93b14b8d5b1e00933bd055175af71a
atalaymurat <atalaymurat@msn.com>
2025-05-07 20:30:23 +0300

    Suspense fix to Contacts

--- frontend/app/contact/page.jsx
+++ frontend/app/contact/page.jsx
@@ -1,7 +1,14 @@
-import ContactPage from "@/components/contact/ContactPage";
+// app/contact/page.js
 
-const Page = () => {
-  return <ContactPage />;
-};
+import { Suspense } from "react";
 
-export default Page;
+// Client-side import için dinamik import kullanıyoruz
+const ContactPage = require("@/components/contact/ContactPage").default;
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="p-8">Sayfa yükleniyor...</div>}>
+      <ContactPage />
+    </Suspense>
+  );
+}
