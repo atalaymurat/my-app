c1c7875c24dd3bb2615e8b5c2503fb3d8c119881
atalaymurat <atalaymurat@msn.com>
2026-04-02 20:18:30 +0300

    changes

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3021",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://192.168.1.100:3021",
   withCredentials: true,
 });
 
